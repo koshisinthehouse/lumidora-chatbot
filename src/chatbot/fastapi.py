5017bff17f2292de4151567861409c9c8ab58c23
Dominik Bruhn <dominikbruhn@googlemail.com>
2023-11-17 14:01:44 +0100

    modified

--- src/chatbot/fastapi.py
+++ src/chatbot/fastapi.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+class BasicPrompt(BaseModel):
+    prompt: str
+
+
+class BasicPromptResopnse(BaseModel):
+    prompt: BasicPrompt
+    answer: str
+
+@app.post("/", response_model=BasicPromptResopnse)
+async def prompt(basicPrompt: BasicPrompt) -> BasicPromptResopnse:
+    return BasicPromptResopnse(prompt=basicPrompt, answer="")

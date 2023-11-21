fd9938640d43c1d2259375e6072381442ccc9260
Dominik Bruhn <dominikbruhn@googlemail.com>
2023-11-21 13:28:57 +0100

    modified

--- src/chatbot/fastapi.py
+++ src/chatbot/fastapi.py
@@ -5,12 +5,13 @@ app = FastAPI()
 
 
 class BasicPrompt(BaseModel):
-    prompt: str
+    prompt: str = "Generiere eine hochwertige und realistische Illustration einer Comicfigur, die als Grundlage für künstliche Intelligenz auf einem YouTube-Kanal dienen soll. Die Figur soll eine fröhliche und freundliche Ausstrahlung haben, gepaart mit einem charismatischen Erscheinungsbild."
 
 
 class BasicPromptResopnse(BaseModel):
     prompt: BasicPrompt
     answer: str
+
 
 @app.post("/", response_model=BasicPromptResopnse)
 async def prompt(basicPrompt: BasicPrompt) -> BasicPromptResopnse:

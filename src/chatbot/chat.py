6e955970aed7ee32869037a33597c68f37f19766
koshisinthehouse@gmail.com <koshisinthehouse@gmail.com>
2023-12-27 20:37:03 +0100

    hiuhuih

--- src/chatbot/chat.py
+++ src/chatbot/chat.py
@@ -6,39 +6,59 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, RetrievalQA
 from langchain.vectorstores.faiss import FAISS
 
+
 class LumidoraChatbotConfiguration:
-    def __init__(self, model_path:str, template:str, text_schemas:list[ResponseSchema]):
-        self.model_path = model_path
-        self.template = template
-        self.text_schemas = text_schemas  # Now handling multiple text schemas
+    def __init__(
+        self, model_path: str, template: str, text_schemas: list[ResponseSchema]
+    ):
+        self.model_path: str = model_path
+        self.template: str = template
+        self.text_schemas: list[
+            ResponseSchema
+        ] = text_schemas  # Now handling multiple text schemas
 
     @classmethod
-    def from_json(cls, json_str:str):
+    def from_json(cls, json_str: str):
         # Parse the JSON string into a dictionary
         config_dict = json.loads(json_str)
         print("Loaded configuration:", config_dict)  # Print the loaded configuration
 
         # Extract and instantiate all text schemas
         text_schemas_dicts = config_dict.pop("text_schemas")
-        text_schemas:list[ResponseSchema] = []
+        text_schemas: list[ResponseSchema] = []
         for schema in text_schemas_dicts:
-            text_schema = ResponseSchema(name=schema["name"], description=schema["description"])
+            text_schema = ResponseSchema(
+                name=schema["name"], description=schema["description"]
+            )
             text_schemas.append(text_schema)
-            print(f"Added text schema: {text_schema.name} - {text_schema.description}")  # Print each text schema
+            print(
+                f"Added text schema: {text_schema.name} - {text_schema.description}"
+            )  # Print each text schema
 
         # Create a new instance of the class using the dictionary
-        return cls(model_path=config_dict["model_path"], template=config_dict["template"], text_schemas=text_schemas)
+        return cls(
+            model_path=config_dict["model_path"],
+            template=config_dict["template"],
+            text_schemas=text_schemas,
+        )
 
 
 class LumidoraChatbot:
     def __init__(self):
-        
         # Open Issue: https://github.com/langchain-ai/langchain/issues/7747
-        self.callbacks = [StreamingStdOutCallbackHandler()]
-            
-    def run_chat(self, input_text:str, config:LumidoraChatbotConfiguration, vectorstore: FAISS|None):
-        
-        print("Configuration loaded with model path:", config.model_path)  # Print model path
+        self.callbacks: list[StreamingStdOutCallbackHandler] = [
+            StreamingStdOutCallbackHandler()
+        ]
+
+    def run_chat(
+        self,
+        input_text: str,
+        config: LumidoraChatbotConfiguration,
+        vectorstore: FAISS | None,
+    ):
+        print(
+            "Configuration loaded with model path:", config.model_path
+        )  # Print model path
 
         # Initialize the LLM with the model from configuration
         self.llm = GPT4All(
@@ -52,37 +72,46 @@ class LumidoraChatbot:
         )
 
         # define response schemas and parser using the text_schemas from configuration
-        self.parser = StructuredOutputParser.from_response_schemas(config.text_schemas)
+        self.parser: StructuredOutputParser = (
+            StructuredOutputParser.from_response_schemas(config.text_schemas)
+        )
         format_instructions = self.parser.get_format_instructions()
 
         # create prompt template
-        prompt = PromptTemplate(template=config.template, input_variables=["context", "question"], partial_variables={"format_instructions": format_instructions})
-        
-        if(vectorstore):
+        prompt = PromptTemplate(
+            template=config.template,
+            input_variables=["context", "question"],
+            partial_variables={"format_instructions": format_instructions},
+        )
+
+        if vectorstore:
             print("use vector store data.")
             retriever = vectorstore.as_retriever()
-            chain_type_kwargs = {"prompt":prompt}
-            qa = RetrievalQA.from_chain_type(llm=self.llm, chain_type="stuff",retriever=retriever, chain_type_kwargs=chain_type_kwargs)
+            chain_type_kwargs: dict[str, PromptTemplate] = {"prompt": prompt}
+            qa = RetrievalQA.from_chain_type(
+                llm=self.llm,
+                chain_type="stuff",
+                retriever=retriever,
+                chain_type_kwargs=chain_type_kwargs,
+            )
             output = qa.run(input_text)
         else:
             print("default query")
             llm_chain = LLMChain(prompt=prompt, llm=self.llm, verbose=True)
-            output =llm_chain(input_text)
-            output = output.get('text')
+            output = llm_chain(input_text)
+            output = output.get("text")
             print(f"output: [[[ {output} ]]]")
-            
+
         # run the chain with input
-    
+
         # parse the output
         parsed_output = self.parser.parse(str(output))
-        #print(f"parsed output: [[[ {parsed_output} ]]]")
+        # print(f"parsed output: [[[ {parsed_output} ]]]")
 
         output_json_string = json.dumps(parsed_output)
-        #print(f"output_json_string: [[[ {output_json_string} ]]]")
+        # print(f"output_json_string: [[[ {output_json_string} ]]]")
 
         json_object = json.loads(output_json_string)
-        #print("final json_object: [[[ {json_object} ]]]")
+        # print("final json_object: [[[ {json_object} ]]]")
 
         return json_object
-
-
--- src/chatbot/embedding.py
+++ src/chatbot/embedding.py
@@ -1,30 +1,39 @@
+from ast import List
 import os
 from langchain.document_loaders import DirectoryLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import GPT4AllEmbeddings
 from langchain.vectorstores.faiss import FAISS
+from typing import List
+
 
 class LumidoraEmbedding:
-
     def __init__(self, datastore_dir: str, vectorstore_dir: str):
         self.datastore_dir = datastore_dir
         self.vectorstore_dir = vectorstore_dir
-    
-    def save_vector_db(self, vectorstore: FAISS):
+
+    def save_vector_db(self, vectorstore: FAISS) -> None:
         # Speichern des Vektorstores mit FAISS-spezifischen Funktionen
         FAISS.save_local(vectorstore, self.vectorstore_dir)
         print(f"Vektorstore gespeichert unter {self.vectorstore_dir}")
-        
-    def create_vector_db(self):
-        loader = DirectoryLoader(self.datastore_dir, glob="**/*.*", loader_cls=TextLoader, show_progress=True,
-                                 loader_kwargs={'autodetect_encoding': True})
-        docs = loader.load()
+
+    def create_vector_db(self) -> None:
+        loader = DirectoryLoader(
+            self.datastore_dir,
+            glob="**/*.*",
+            loader_cls=TextLoader,
+            show_progress=True,
+            loader_kwargs={"autodetect_encoding": True},
+        )
+        docs: List = loader.load()
         print("Dokumente geladen.")
 
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=500, chunk_overlap=100
+        )
         documents = text_splitter.split_documents(docs)
         print("Chunks erstellt.")
-        embeddings = GPT4AllEmbeddings() # type: ignore
+        embeddings = GPT4AllEmbeddings()  # type: ignore
         vectorstore: FAISS = FAISS.from_documents(documents, embeddings)
         print("Vektorstore erstellt aus Dokumenten.")
 
@@ -34,8 +43,12 @@ class LumidoraEmbedding:
         # Überprüfen, ob das Verzeichnis existiert
         if os.path.exists(self.vectorstore_dir) and os.path.isdir(self.vectorstore_dir):
             embeddings = GPT4AllEmbeddings()  # Erstellen der Embeddings
-            return FAISS.load_local(folder_path=self.vectorstore_dir, embeddings=embeddings)
+            return FAISS.load_local(
+                folder_path=self.vectorstore_dir, embeddings=embeddings
+            )
         else:
             # Wenn das Verzeichnis nicht existiert oder kein Verzeichnis ist, None zurückgeben
-            print(f"Verzeichnis {self.vectorstore_dir} existiert nicht oder ist kein Verzeichnis.")
+            print(
+                f"Verzeichnis {self.vectorstore_dir} existiert nicht oder ist kein Verzeichnis."
+            )
             return None
